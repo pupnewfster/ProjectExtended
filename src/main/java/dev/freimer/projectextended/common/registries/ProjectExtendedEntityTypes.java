31e45394a34b6a42119a06d9caad287c30b03e93
Sara Freimer <sara@freimer.com>
2025-03-17 10:18:55 -0500

    Fix crash on load

--- src/main/java/dev/freimer/projectextended/common/registries/ProjectExtendedEntityTypes.java
+++ src/main/java/dev/freimer/projectextended/common/registries/ProjectExtendedEntityTypes.java
@@ -5,14 +5,12 @@ import dev.freimer.projectextended.common.entity.PETridentEntity;
 import moze_intel.projecte.gameObjs.registration.impl.EntityTypeDeferredRegister;
 import moze_intel.projecte.gameObjs.registration.impl.EntityTypeRegistryObject;
 import net.minecraft.SharedConstants;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
 
 public class ProjectExtendedEntityTypes {
 
     public static final EntityTypeDeferredRegister ENTITY_TYPES = new EntityTypeDeferredRegister(ProjectExtended.MODID);
 
-    public static final EntityTypeRegistryObject<PETridentEntity> PE_TRIDENT = ENTITY_TYPES.register("pe_trident", EntityType.Builder.<PETridentEntity>of(PETridentEntity::new, MobCategory.MISC)
+    public static final EntityTypeRegistryObject<PETridentEntity> PE_TRIDENT = ENTITY_TYPES.registerMisc("pe_trident", PETridentEntity::new, builder -> builder
           .sized(0.5F, 0.5F)
           .eyeHeight(0.13F)
           .clientTrackingRange(4)
